6a247b182f0fb96e417197ddeb64aeede8fdf834
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-05 13:53:25 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//scopo gioco: 
+//quando clicco sul pulsante ON mostro l'immagine della lampadina accesa
+//quando clicco sul pulsante OFF mostro l'immagine della lampadina spenta
+
+//dati da raccogliere :
+//immagine, pulsante ON, pulsante OFF
+
+const btnON = document.getElementById("button-on")
+const btnOFF = document.getElementById("button-off")
+let imgElem = document.querySelector("img");
