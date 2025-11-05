3f4b5f298e7778a936ee84c2bdc3aca81ce955da
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-05 15:44:27 +0100

    FEAT: bonus completato wip: stile pagina

--- js/script.js
+++ js/script.js
@@ -16,3 +16,42 @@ btnON.addEventListener("click", function() {
 btnOFF.addEventListener("click", function () {
     imgElem.src = "./img/white_lamp.png"
 })
+
+
+///////////////
+// BONUS
+////////////////
+
+//scopo gioco: quando clicco il pulsante accendi cambio l'immagine della lampadina
+// e cambio lo stile del pulsante (testo al suo interno e il colore di sfondo)
+//dati da raccogliere: pulsante accendi, immagine (che ho già preso su^)
+//      per fare in modo che premendo lo stesso pulsante si verifichino le condizioni sopra citate
+//      dichiaro una variabile e le assegno il valore di vero come predefinito 
+//      e cambiando il valore di questa variabile si verificheranno i vari cambiamenti 
+
+const btnOR = document.getElementById("button-or")
+const imgOR = document.getElementById("img-or")
+
+//ICONE BOOTSTRAP 
+const iconON = document.getElementById("icon-on")
+const iconOFF = document.getElementById("icon-off");
+
+
+let isON = true;
+
+btnOR.addEventListener("click", function () {
+    if(isON === true) {
+        imgOR.src = "./img/yellow_lamp.png"
+        btnOR.innerHTML = "SPEGNI";
+        btnOR.classList.remove("btn-success");
+        btnOR.classList.add("btn-secondary")
+    }else {
+        imgOR.src = "./img/white_lamp.png"
+        btnOR.innerText = "Accendi";
+        btnOR.classList.add("btn-success");
+    }
+    isON = !isON;
+    
+})
+
+
